1fba44a8ba89ff844ddcb22a71ad829374d14cf5
JavierFeFe <44543081+JavierFeFe@users.noreply.github.com>
2018-10-28 18:09:11 +0100

    Add files via upload

---
+++ Ejercicio2/colaborar.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+public class colaborar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombreArchivo = "prueba.txt"; //Valor por defecto
+		int repeticiones = 10; //Valor por defecto
+		if (args.length == 2 && args[0].matches("\\d+")) { //Comprueba que los parámetros introducidos sean correctos
+			nombreArchivo = args[1];
+			repeticiones = Integer.parseInt(args[0]);
+		}else { 
+			//return; //Si activo este return solo funcionaría si se introducen parámetros
+		}
+		for (int i=0; i<repeticiones; i++) { //Bucle que ejecuta la tarea con las repeticiones designadas
+			try {
+				int lineas = (i+1) * 10; //Incrementa el nº de líneas en 10
+				System.out.println("Ejecutando aplicación que generará " + lineas + " líneas.");
+				Runtime.getRuntime().exec("java -jar lenguaje.jar " + lineas + " " + nombreArchivo ); //Ejecuta lenguaje.jar con los parámetros designados
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
