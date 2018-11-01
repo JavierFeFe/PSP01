f42e27ad6fed231f2d86f5929f790e52a48144fc
JavierFeFe <44543081+JavierFeFe@users.noreply.github.com>
2018-11-01 16:39:39 +0100

    Update colaborar.java

--- Ejercicio2/colaborar.java
+++ Ejercicio2/colaborar.java
@@ -1,3 +1,5 @@
+package Ejercicio2;
+
 import java.io.IOException;
 
 public class colaborar {
@@ -6,17 +8,17 @@ public class colaborar {
 		// TODO Auto-generated method stub
 		String nombreArchivo = "prueba.txt"; //Valor por defecto
 		int repeticiones = 10; //Valor por defecto
-		if (args.length == 2 && args[0].matches("\\d+")) { //Comprueba que los par·metros introducidos sean correctos
+		if (args.length == 2 && args[0].matches("\\d+")) { //Comprueba que los par√°metros introducidos sean correctos
 			nombreArchivo = args[1];
 			repeticiones = Integer.parseInt(args[0]);
 		}else { 
-			//return; //Si activo este return solo funcionarÌa si se introducen par·metros
+			//return; //Si activo este return solo funcionar√≠a si se introducen par√°metros
 		}
 		for (int i=0; i<repeticiones; i++) { //Bucle que ejecuta la tarea con las repeticiones designadas
 			try {
-				int lineas = (i+1) * 10; //Incrementa el n∫ de lÌneas en 10
-				System.out.println("Ejecutando aplicaciÛn que generar· " + lineas + " lÌneas.");
-				Runtime.getRuntime().exec("java -jar lenguaje.jar " + lineas + " " + nombreArchivo ); //Ejecuta lenguaje.jar con los par·metros designados
+				int lineas = (i+1) * 10; //Incrementa el n¬∫ de l√≠neas en 10
+				System.out.println("Ejecutando aplicaci√≥n que generar√° " + lineas + " l√≠neas.");
+				Runtime.getRuntime().exec("java -jar lenguaje.jar " + lineas + " " + nombreArchivo ); //Ejecuta lenguaje.jar con los par√°metros designados
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
