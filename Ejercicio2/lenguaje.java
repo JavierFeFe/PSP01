b3d30a8f14b4cd0f3e97480940e1da9c8b69e0f9
JavierFeFe <44543081+JavierFeFe@users.noreply.github.com>
2018-11-04 20:21:01 +0100

    Update lenguaje.java

--- Ejercicio2/lenguaje.java
+++ Ejercicio2/lenguaje.java
@@ -12,10 +12,10 @@ public class lenguaje {
 	private static RandomAccessFile raf;
 	private static FileLock bloqueo;
 	
-	//Java -jar lenguaje.jar prueba.txt 100 //Ejemplo de uso con parámetros
+	//Java -jar lenguaje.jar prueba.txt 100 //Ejemplo de uso con parÃ¡metros
 	
 	public static void main(String[] args) {
-		// ASCII letras minúsculas del 97 al 122
+		// ASCII letras minÃºsculas del 97 al 122
 		String nombreArchivo = "prueba.txt";
 		int lineas = 100;
 		if (args.length == 2 && args[0].matches("\\d+")) {
@@ -26,23 +26,23 @@ public class lenguaje {
 		}
 		try {
 			archivo = new File(nombreArchivo);			
-			for (int i = 0; i<lineas; i++) { //generará el nº de líneas indicadas en la variable lineas
+			for (int i = 0; i<lineas; i++) { //generarÃ¡ el nÂº de lÃ­neas indicadas en la variable lineas
 				Random ascii = new Random(); 
 				Random numeroLetras = new Random();
-				int longitud = numeroLetras.nextInt(8)+7; //genera una cadena de texto con letras aleatorias con un mínimo de 7 letras y un máximo de 15
+				int longitud = numeroLetras.nextInt(8)+7; //genera una cadena de texto con letras aleatorias con un mÃ­nimo de 7 letras y un mÃ¡ximo de 15
 				String cadena = "";
 				for (int o = 0; o < longitud; o++) {
-					char  letra = (char) (ascii.nextInt(26) + 97); //genera un caracter que se encuentre entre la posición 97 y 122 en el código ascii
-					cadena += letra; //añade ese caracter a la cadena de texto
+					char  letra = (char) (ascii.nextInt(26) + 97); //genera un caracter que se encuentre entre la posiciÃ³n 97 y 122 en el cÃ³digo ascii
+					cadena += letra; //aÃ±ade ese caracter a la cadena de texto
 				}
 				raf = new RandomAccessFile(archivo, "rwd"); //Creamos un acceso al archivo
 				//while ((bloqueo=raf.getChannel().tryLock()) == null); //Intenta desbloquear el archivo, si no puede se queda en espera
 				bloqueo=raf.getChannel().lock(); //Se queda a la espera para desbloquear el archivo
-				while (raf.readLine() != null); //Se mueve al final del archivo				
-				if (raf.getFilePointer() == 0) { //Si está en la posición del primer caracter no añade salto de línea al principio
+				raf.seek(raf.length());//Se mueve al final del archivo				
+				if (raf.getFilePointer() == 0) { //Si estÃ¡ en la posiciÃ³n del primer caracter no aÃ±ade salto de lÃ­nea al principio
 					raf.writeBytes(cadena);
 				}else {
-					raf.writeBytes("\r\n" + cadena); //Es necesario hacerlo así para q no añada un salto de línea al final del archivo
+					raf.writeBytes("\r\n" + cadena); //Es necesario hacerlo asÃ­ para q no aÃ±ada un salto de lÃ­nea al final del archivo
 				}
 				raf.seek(0); //Nos posiciona al principio del archivo
 				bloqueo.release();//Libera el bloqueo
@@ -56,7 +56,7 @@ public class lenguaje {
 		}finally {
 			try {
 				if (bloqueo != null) {
-					bloqueo.release(); //Se libera el acceso al archivo aunque falle la aplicación
+					bloqueo.release(); //Se libera el acceso al archivo aunque falle la aplicaciÃ³n
 				}if (raf != null) raf.close(); // Se cierra el acceso
 			}catch (IOException e) {
 				e.printStackTrace();
